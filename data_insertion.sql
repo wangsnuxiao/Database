292fb21871199d20486fd338ebdd0cf61e2bfe0e
xuanhuaz <kaoowfeixiang@gmail.com>
2022-04-09 14:46:40 -0400

    User data

---
+++ data_insertion.sql
@@ -0,0 +1,8 @@
+
+-- Insertion for User Table
+insert into User (uid, username, password, email, city, state) values
+    (null, 'Charlie','111111','xxxx@nyu.edu','newyork','NY'),
+    (null, 'Alex', '123456','alex@nyu.edu', 'newport', 'NJ'),
+    (null, 'Billy', '654321', 'billty@mit.edu','boston', 'MA'),
+    (null, 'Dogge', '1241512', 'dog@njit.edu', 'newport', 'NJ'),
+    (null, 'Eilly', 'jvoiwenq', 'eilly@buffalo.edu', 'buffalo','NY');
