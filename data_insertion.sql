a7c01359b344880b3ae3c39df9bfdb7fc5b40294
djidcvdna <86199292+djidcvdna@users.noreply.github.com>
2022-04-09 14:48:25 -0400

    Update data_insertion.sql

---
+++ data_insertion.sql
@@ -0,0 +1,8 @@
+
+insert into Profile(uid, visibility, age, date_of_birth, description, level, total_thumbs_up)
+values  (1,1,22,'1999-5-24','this is des','noob',0),
+        (1,0,12,'1999-5-24','this is des','noob',0),
+        (1,0,25,'1999-5-24','this is des','noob',0),
+        (1,1,30,'1999-5-24','this is des','noob',0),
+        (1,1,70,'1999-5-24','this is des','noob',0)
+
