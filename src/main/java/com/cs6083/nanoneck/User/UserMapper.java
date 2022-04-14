c66d5bceb5dd169ddc8c051fd8f5007a745077d1
xuanhuaz <kaoowfeixiang@gmail.com>
2022-04-14 17:54:42 -0400

    deleted UserRespository

--- src/main/java/com/cs6083/nanoneck/User/UserMapper.java
+++ src/main/java/com/cs6083/nanoneck/User/UserMapper.java
@@ -1,10 +1,23 @@
 package com.cs6083.nanoneck.User;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
 public interface UserMapper {
+    @Select("select * from User")
+    List<User> queryUserList();
 
+    @Select("select * from User where uid = #{id}")
+    User queryUserById(int id);
+
+    int addUser(User user);
+
+    int updateUser(User user);
+
+    int deleteUser(int id);
 }
