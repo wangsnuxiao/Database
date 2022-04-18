3cf6c21d4a0be98afb6407b480961d70596121e9
djidcvdna <86199292+djidcvdna@users.noreply.github.com>
2022-04-18 12:03:49 -0400

    Data update

--- data_insertion.sql
+++ data_insertion.sql
@@ -10,10 +10,10 @@ insert into User (uid, username, password, email, city, state) values
 -- Insertion for Profile table
 insert into Profile(uid, visibility, age, date_of_birth, description, level, total_thumbs_up)
 values  (1,1,22,'1999-5-24','this is des','noob',0),
-        (2,0,12,'1999-5-24','this is des','noob',0),
-        (3,0,25,'1999-5-24','this is des','noob',0),
-        (4,1,30,'1999-5-24','this is des','noob',0),
-        (5,1,70,'1999-5-24','this is des','noob',0);
+        (2,0,23,'1998-7-24','this is des','noob',0),
+        (3,0,21,'2000-12-24','this is des','noob',0),
+        (4,1,20,'2001-3-24','this is des','noob',0),
+        (5,1,19,'2002-7-11','this is des','noob',0);
 
 -- Insertion for root Topics table
 insert into Topics(tid, tname, parent_id)
